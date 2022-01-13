322b6cc14aa995b6a946d2c971e6eb6a0a0cca1f
dakshdua1234 <88582525+dakshdua1234@users.noreply.github.com>
2022-01-13 19:12:26 +0530

    Add files via upload

---
+++ project 26.js
@@ -0,0 +1,34 @@
+class rope{
+	constructor(body1,body2, offsetX)
+	{
+	this.offstex.offsetX
+	this.offsety.offsety
+	varoptions=(
+		bodyA:body1,
+		bodyB:body2,
+		point0(x:this.offsetx,y:this.offsety)
+	}
+	//console.log(options);
+	this.rope-Constraint.create(options)
+	world.add(world,this,rope)
+}
+
+	display()
+	(
+	varpointA.this.rope,bodyA.position;
+	varpointB.this.rope,bodyB.position;
+
+	strokeWeight(2);
+	
+ varAnchor1X.pointA.x
+ varAnchor1Y.pointB.y
+
+var Anchor2X.point0x-this.offsetx
+var Anchor2Y.point0y-this.offsety
+
+line(anchor1x,varAnchor1Y,Anchor2X,Anchor2Y)
+	}
+}
+		
+	
+
